20efac953f07c9955d611bf61dc1b8838fea7a02
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-08-08 21:09:00 +0300

    Exercise 1

---
+++ EX_1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите 1 число = ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число = ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (number1 > number2)
+{
+    Console.WriteLine("max = ");
+    Console.WriteLine(number1);
+
+}
+else if (number2 > number1)
+{
+    Console.WriteLine("max = ");
+    Console.WriteLine(number2);
+
+}
