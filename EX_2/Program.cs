bec8c36c8a8feb1cea940f34e124ff29861d67b5
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-08-08 21:15:33 +0300

    Exercise 2

---
+++ EX_2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите 1 число = ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число = ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 число = ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+
+if (number1 > number2 && number1 > number3)
+
+{
+    Console.WriteLine("max = ");
+    Console.WriteLine(number1);
+}
+
+else if (number2 > number3)
+
+{
+    Console.WriteLine("max = ");
+    Console.WriteLine(number2);
+}
+
+else
+{
+    Console.WriteLine("max = ");
+    Console.WriteLine(number3);
+}
